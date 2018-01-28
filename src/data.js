dcb6b8f447a63ebeaa597a98c5092296236b27f5
Unknown <willem.delbare@gmail.com>
2018-01-28 01:30:37 +0100

    - import first checklist version

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 export const steps = [
   {
     id: 'section-1',
-    title:'Section 1',
+    title:'Information you hold',
     items: [{ 
         id: 1, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Your company has a list of places where it keeps personal information and the ways data flows between them', 
+        role: ['processor', 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -13,8 +13,8 @@ export const steps = [
       },
       { 
         id: 2, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Your company has a list of all types of personal information it holds, the source of that information, who you share it with, what you do with it and how long you will keep it', 
+        role: ['processor', 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -22,8 +22,17 @@ export const steps = [
       },
       { 
         id: 3, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Your company has a publicy accessible privacy policy that outlines all you processes related to personal data. ', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 4, 
+        title: 'Your privacy policy should include a lawful basis to explain why the company needs to process personal information', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -33,11 +42,11 @@ export const steps = [
   },
   {
     id: 'section-2',
-    title:'Section 2',
+    title:'Accountability & management',
     items: [{ 
         id: 1, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Your company has appointed a Data Protection Officer', 
+        role: ['processor', 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -45,8 +54,8 @@ export const steps = [
       },
       { 
         id: 2, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Create awareness among decision makers about GPDR guidelines', 
+        role: ['processor', 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -54,8 +63,51 @@ export const steps = [
       },
       { 
         id: 3, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Make sure your technical security is up to date. ', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 4, 
+        title: 'Train staff to be aware of data protection', 
+        role: ['processor'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 5, 
+        title: 'You have a list of sub-processors and your privacy policy mentions your use of this sub-processor', 
+        role: ['processor'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 6, 
+        title: 'If your business operates outside the EU, you have appointed a representative within the EU.', 
+        role: ['processor'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },{ 
+        id: 7, 
+        title: 'You report data breaches involving personal data to the local authority ', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },{ 
+        id: 8, 
+        title: 'There is a contract in place with any data processors that you share data with ', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -65,11 +117,12 @@ export const steps = [
   },
   {
     id: 'section-3',
-    title:'Section 3',
-    items: [{ 
+    title:'Exercise of new rights',
+    items: [,
+      { 
         id: 1, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Your customers can easily request access to their personal information', 
+        role: ['processor', 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -77,8 +130,8 @@ export const steps = [
       },
       { 
         id: 2, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Your customers can easily update their own personal information to keep it accurate', 
+        role: ['processor', 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -86,8 +139,53 @@ export const steps = [
       },
       { 
         id: 3, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'You automatically delete data that your business no longer has any use for', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 4, 
+        title: 'Your customers can easily request deletion of their personal data', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 5, 
+        title: 'Your customers can easily request that you stop processing their data', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 6, 
+        title: 'Your customers can easily request that their data be delivered to themselves or a 3rd party', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 7, 
+        title: 'Your customers can easily object the processing of their data', 
+        role: ['controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 8, 
+        title: 'Your customers can easily object to profiling or automated decision making that could impact them', 
+        role: ['controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -97,11 +195,11 @@ export const steps = [
   },
   {
     id: 'section-4',
-    title:'Section 4',
+    title:'Consent',
     items: [{ 
         id: 1, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Ask consent when you start processing information about a person', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -109,8 +207,8 @@ export const steps = [
       },
       { 
         id: 2, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'Your privacy policy should be written in clear and understandable terms ', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -118,8 +216,26 @@ export const steps = [
       },
       { 
         id: 3, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'It should be as easy for your customers to withdraw consent as it was to give it in the first place', 
+        role: [ 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 4, 
+        title: 'If you process children\'s personal data, verify their age and aks their consent', 
+        role: [ 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 5, 
+        title: 'When you update your privacy policy, you inform existing customers', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -129,11 +245,11 @@ export const steps = [
   },
   {
     id: 'section-5',
-    title:'Section 5',
+    title:'Continuous follow-up',
     items: [{ 
         id: 1, 
-        title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        title: 'You regularly review policies for changes, effectiveness, changes in handling of data, changes to the state of affairs of other countries your data flows to,..', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -142,7 +258,7 @@ export const steps = [
       { 
         id: 2, 
         title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
@@ -151,12 +267,35 @@ export const steps = [
       { 
         id: 3, 
         title: 'Ensure your domain names are secured', 
-        role: 'controller', 
+        role: [ 'controller'], 
         description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
         links: [
           'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
         ]  
       } 
+    ]
+  },
+  {
+    id: 'section-6',
+    title:'Special cases',
+    items: [{ 
+        id: 1, 
+        title: 'Your business understands when you must conduct a DPIA for high-risk processing of sensitivie data', 
+        role: [ 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      },
+      { 
+        id: 2, 
+        title: 'You should only transfer data outside of the EU to countries that offer an appropriate level of protection', 
+        role: ['processor', 'controller'], 
+        description: 'Domain names should be renewed regularly. If you have bought one from a third party, you should also make sure that the authoritative configured name server is your own.',
+        links: [
+          'http://www.esecurityplanet.com/views/article.php/3928456/8-Tips-for-Protecting-Your-Domain-Names.htm'
+        ]  
+      }
     ]
   }
 ]

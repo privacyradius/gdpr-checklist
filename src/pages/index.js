99f2dee0055b992b25a079c3272d4b3ae8678151
Unknown <willem.delbare@gmail.com>
2018-01-28 01:31:04 +0100

    - make sure filter buttons work

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ class Li extends React.Component {
     super()
     this.state = {
       isChecked: false,
-      isExpended: false
+      isExpended: false,
     }
   }
 
@@ -44,7 +44,12 @@ class Li extends React.Component {
           </div>
           <div className={`expend-bar ${this.state.isChecked ? 'checked' : ''}`} onClick={this.handleExpend}>
             <p>{this.props.title}</p>
-            <span className="category">{this.props.role}</span>
+           
+                 {this.props.role.map(function(role, index){
+                     
+                     return <span className="category" key={ index }>{role}</span>;
+                  })}
+           
           </div>
           <div className="btn">
             <img src={Arrow} alt="" className="arrow" onClick={this.handleExpend} />
@@ -61,41 +66,107 @@ class Li extends React.Component {
   }
 }
 
-const Section = props => (
-  <div className="scrollspy" id={[props.id]}>
-    <h2>{props.title}</h2>
-    <ul className="checklist">
-      { props.items.map((l, index) => <Li {...l} top={index * 70} />)}
-    </ul>
-  </div>
-)
+class Section extends React.Component {
+    
+  render () {
+    return (
+        
+        <div>
+        {
+        
+        this.props.list.map(function(item, index){
+            
+            var filtered_list = [];
+            item.items.map( function(l, index)
+                          {
+                            if( ( this.props.controllerSelected  && l.role.includes('controller')  ) ||
+                                    ( this.props.processorSelected  && l.role.includes('processor')  )  )
+                            
+                            {
+                                filtered_list.push( l );
+                            }
+                            
+                          }.bind(this)
+              )
+                      
+             if( filtered_list.length == 0)
+             {
+                return ;
+             }
+             return   <div className="scrollspy" id={[item.id]}>
+                    <h2>{item.title}</h2>
+                    <ul className="checklist">
+                      { filtered_list.map( function(l, index)
+                          {
+                            if( ( this.props.controllerSelected  && l.role.includes('controller')  ) ||
+                                    ( this.props.processorSelected  && l.role.includes('processor')  )  )
+                            
+                            {
+                                return <Li {...l} top={index * 70} />
+                            }
+                            
+                          }.bind(this)
+                      )}
+                    </ul>
+                  </div>
+                  
+          }.bind(this) )}
+        
+        </div>
+    )
+  }
 
-const IndexPage = () => (
-  <div>
-    <div className='wrapper'>
-      <div className='columns'>
-        <Sidebar />
-        <div className="col-9">
-          <h1>The GDPR Checklist</h1>
-          <p className="description">Achieving GDPR Compliance shouldn't feel like a struggle. 
-          This is a basic checklist you can use to harden your GDPR compliancy. Select if you are a data processor 
-          (explain) or data controllor (explain) and use the checkpoints to improve your compliancy. 
-          This list is far from a legal exhaustive document, it merely tries to help you overcome the struggle.
-          <br/><br/>Feel free to <a href="https://github.com/GDPRTracker/checklist" target="_blank">contribute directly</a> on GitHub!</p>
-          <div className="filter-bar">
-            <h3>ROLE</h3>
-            <ul className="selected-three">
-              <li className="seed">Data Controller</li>
-              <li className="seriesa">Data Processor</li>
-            </ul>
+}
+
+class IndexPage extends React.Component {
+    
+  constructor() {
+    super()
+    this.state = {
+      processorSelected: true,
+      controllerSelected: true
+    }
+  }
+  toggleController = () => {
+     this.setState({ controllerSelected : !this.state.controllerSelected } );
+  }
+  
+  toggleProcessor = () => {
+     this.setState({ processorSelected : !this.state.processorSelected } );
+  }
+  
+  render () {
+    return (
+      <div>
+        <div className='wrapper'>
+          <div className='columns'>
+            <Sidebar />
+            <div className="col-9">
+              <h1>The GDPR Checklist</h1>
+              <p className="description">Achieving GDPR Compliance shouldn't feel like a struggle. 
+              This is a basic checklist you can use to harden your GDPR compliancy. Select if you are a data processor 
+              (explain) or data controllor (explain) and use the checkpoints to improve your compliancy. 
+              This list is far from a legal exhaustive document, it merely tries to help you overcome the struggle.
+              <br/><br/>Feel free to <a href="https://github.com/GDPRTracker/checklist" target="_blank">contribute directly</a> on GitHub!</p>
+              <div className="filter-bar">
+                <h3>ROLE</h3>
+                <ul className="selected-three">
+                  <li onClick={this.toggleController} className={this.state.controllerSelected ? 'is-active' : ''}>Data Controller</li>
+                  <li onClick={this.toggleProcessor} className={this.state.processorSelected ? 'is-active' : ''}>Data Processor</li>
+                </ul>
+              </div>
+                { steps.map( (function(s)
+                    {
+                    return <Section list={[s]} controllerSelected={this.state.controllerSelected} processorSelected={this.state.processorSelected} /> 
+                    }).bind(this)
+                    ) }
+              <Newsletter />
+              {/* <Footer /> */}
+            </div>
           </div>
-          { steps.map(s => <Section {...s} />)}
-          <Newsletter />
-          {/* <Footer /> */}
         </div>
       </div>
-    </div>
-  </div>
-)
+  )}
+}
 
 export default IndexPage

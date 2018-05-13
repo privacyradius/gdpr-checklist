151bb1e4c10dcb8cef782910679d85e25d9337ac
Gertjan De Wilde <info@gertjandewilde.com>
2018-05-13 02:27:27 +0200

    Replace Drip with Mailchimp

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,19 +2,8 @@ import React from 'react'
 
 const Newsletter = props => (
   <div className="newsletter">
-    <h4>Stay up to date with major changes in the list</h4>
-    <div className="subtitle">Get notified about major updates to the list:</div>
-    <form className="contact-form" action="https://www.getdrip.com/forms/840769526/submissions" method="post">
-      <fieldset>
-        <input type="email" name="fields[email]" placeholder="Email address" required="" />
-        <input type="text" name="fields[name]" placeholder="Name" required="" />
-        <input type="text" name="fields[company]" placeholder="Company" required="" />
-      </fieldset>
-      <span className="text"></span>
-      <button className="submit-button" name="submit" data-drip-attribute="sign-up-button" value="Submit">
-        Submit
-      </button>
-    </form>
+    <h4>Stay up to date with major changes in the list.</h4>
+    <a href="http://eepurl.com/duyFHH" className="submit-button">Sign up for updates</a>
   </div>
 )
 
